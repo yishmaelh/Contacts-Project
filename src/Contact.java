2ae955a6017f777183214fdf7dc7d883826b80cd
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-13 14:31:10 -0500

    working on constructors

--- src/Contact.java
+++ src/Contact.java
@@ -2,4 +2,9 @@ public class Contact {
 
     private String contactName;
     private String phoneNumber;
+
+    public Contact(String contactName, String phoneNumber){
+        this.contactName = contactName;
+        this.phoneNumber = phoneNumber;
+    }
 }
