daea99b87724a7a66de593828cc227667dd320cc
Eli <84743591+Eliguajardo@users.noreply.github.com>
2021-08-13 15:30:27 -0500

    added arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        ArrayList<String> contactInfo = new ArrayList<String>();
+
+        contactInfo.add("say Your going to have to much fun with the say command");
+
+
+    }
+
+
+
+
 }
