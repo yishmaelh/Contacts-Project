485975a91b97d0b16b4674c31e822545b9b3cdd1
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-16 14:25:59 -0500

    more tweaks

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
          boolean continueRunning = true;
-
-        ContactsApp.mainMenu();
+         
 
         do {
             int userSelection = ContactsApp.mainMenu();
