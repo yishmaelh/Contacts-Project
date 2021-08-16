1e1485eff207687aadf9787adb661e34fe2c6e7b
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-16 09:12:15 -0500

    main menu created

--- src/ContactApp.java
+++
@@ -1,5 +0,0 @@
-public class ContactApp {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/ContactsApp.java
@@ -0,0 +1,11 @@
+public class ContactsApp {
+
+    public static void mainMenu(){
+        System.out.println("Please make a selection");
+        System.out.println(" 1.View contacts");
+        System.out.println(" 2. Add new contact");
+        System.out.println(" 3. Search contact by name or number");
+        System.out.println(" 4. Delete a contact");
+        System.out.println(" 5. Exit application");
+    }
+}
